28f0f846d0a5667c1d69307ea64f520032ac81c3
tasuke22 <tasukesuke@gmail.com>
2022-11-08 23:00:29 +0900

    ✨ headerの追加

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/Link";
+import classes from "./Header.module.css";
+
+export function Header() {
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        {" "}
+        <a href="/" className={classes.anchor}>
+          Index
+        </a>{" "}
+      </Link>
+      <Link href="/about">
+        {" "}
+        <a href="/" className={classes.anchor}>
+          About
+        </a>{" "}
+      </Link>
+    </header>
+  );
+}
