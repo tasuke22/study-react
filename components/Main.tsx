30aae6812661e1df8fed9ccb6714b2a0bf334afd
tasuke22 <tasukesuke@gmail.com>
2022-11-08 22:25:22 +0900

    ♻️

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 import { Links } from "./Links";
 import { Headline } from "./Headline";
 
 export function Main(props: any) {
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <Headline
         page={props.page}
-        code={<code className={styles.code}>pages/{props.page}.js</code>}
+        code={<code className={classes.code}>pages/{props.page}.js</code>}
       ></Headline>
       <Links />
     </main>
