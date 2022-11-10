bc5cff9d9ca1337f0613ce998993b8c256299420
tasuke22 <tasukesuke@gmail.com>
2022-11-10 22:30:11 +0900

    🐛

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,17 +5,11 @@ import classes from "./Header.module.css";
 export function Header() {
   return (
     <header className={classes.header}>
-      <Link href="/">
-        {" "}
-        <a href="/" className={classes.anchor}>
-          Index
-        </a>{" "}
+      <Link href="/" className={classes.anchor}>
+        Index
       </Link>
-      <Link href="/about">
-        {" "}
-        <a href="/" className={classes.anchor}>
-          About
-        </a>{" "}
+      <Link href="/about" className={classes.anchor}>
+        About
       </Link>
     </header>
   );
